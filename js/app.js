96a67b0394f7b532aae20aafb0985fbacea4271a
`foxslot` <`foxslot777@gmail.com`>
2020-11-09 20:53:54 +0300

    delete click

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ let score = 0;
 
 //действия при нажатии кнопки
 document.addEventListener(`keydown`, moveUp);
-document.addEventListener(`click`, moveUp);
+//document.addEventListener(`click`, moveUp);
 //document.addEventListener(`touchstart`, moveUp);
 
 function moveUp() {
